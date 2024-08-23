b25bde84076ef8ab8033235b46ea3a45ba0487c1
unknown <adhovi@gmail.com>
2024-08-24 02:44:07 +0600

    error solved

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require("express");
 
 const cors = require("cors");
-const MongoClient = require("mongodb").MongoClient;
+const {
+  MongoClient,
+  ServerApiVersion
+} = require('mongodb');
 const ObjectId = require("mongodb").ObjectID;
+const bodyParser = require("body-parser");
 
 require("dotenv").config();
 
@@ -10,142 +14,172 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded());
-app.use(bodyParser.urlencoded({ extended : true }));
+app.use(express.urlencoded({
+  extended: true
+}));
 
 app.get("/", function (req, res) {
   res.send("Welcome to Corona-Care Server");
 });
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6qte7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6qte7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
 });
-client.connect((err) => {
-  const bookingsCollection = client
-    .db(process.env.DB_NAME)
-    .collection("bookings");
 
-  app.post("/addBooking", (req, res) => {
-    const newBooking = req.body;
-    bookingsCollection.insertOne(newBooking).then((result) => {
-      res.send(result.insertedCount > 0);
-    });
-  });
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    const bookingsCollection = client
+      .db(process.env.DB_NAME)
+      .collection("bookings");
 
-  app.get("/bookings", (req, res) => {
-    bookingsCollection
-      .find({ email: req.query.email })
-      .toArray((err, items) => {
-        res.send(items);
+    app.post("/addBooking", (req, res) => {
+      const newBooking = req.body;
+      bookingsCollection.insertOne(newBooking).then((result) => {
+        res.send(result.insertedCount > 0);
       });
-  });
-
-  app.get("/orders", (req, res) => {
-    bookingsCollection.find().toArray((err, items) => {
-      res.send(items);
     });
-  });
 
-  app.post("/updateOrderStatus", (req, res) => {
-    const order = req.body;
-    console.log(order);
-    bookingsCollection.updateOne(
-      { _id: ObjectId(order.id) },
-      {
-        $set: { status: order.status },
-      },
-      (err, result) => {
-        if (err) {
-          console.log(err);
-          res.status(500).send({ message: err });
-        } else {
-          res.send(result);
-          console.log(result);
+    app.get("/bookings", (req, res) => {
+      bookingsCollection
+        .find({
+          email: req.query.email
+        })
+        .toArray((err, items) => {
+          res.send(items);
+        });
+    });
+
+    app.get("/orders", (req, res) => {
+      bookingsCollection.find().toArray((err, items) => {
+        console.log(items);
+      });
+    });
+
+    app.post("/updateOrderStatus", (req, res) => {
+      const order = req.body;
+      console.log(order);
+      bookingsCollection.updateOne({
+          _id: ObjectId(order.id)
+        }, {
+          $set: {
+            status: order.status
+          },
+        },
+        (err, result) => {
+          if (err) {
+            console.log(err);
+            res.status(500).send({
+              message: err
+            });
+          } else {
+            res.send(result);
+            console.log(result);
+          }
         }
-      }
-    );
-  });
-
-  //******************
-  // Bookings From Here
-  //******************
-
-  const servicesCollection = client
-    .db(process.env.DB_NAME)
-    .collection("services");
-
-  app.get("/services", (req, res) => {
-    servicesCollection.find().toArray((err, items) => {
-      res.send(items);
+      );
     });
-  });
 
-  app.get("/service/:id", (req, res) => {
-    servicesCollection
-      .find({ _id: ObjectId(req.params.id) })
-      .toArray((err, items) => {
+    //******************
+    // Bookings From Here
+    //******************
+
+    const servicesCollection = client
+      .db(process.env.DB_NAME)
+      .collection("services");
+
+    app.get("/services", (req, res) => {
+      servicesCollection.find().toArray((err, items) => {
         res.send(items);
       });
-  });
-
-  app.post("/addService", (req, res) => {
-    const newService = req.body;
-    servicesCollection.insertOne(newService).then((result) => {
-      res.send(result.insertedCount > 0);
     });
-  });
 
-  app.delete("/deleteService/:id", (req, res) => {
-    servicesCollection
-      .deleteOne({ _id: ObjectId(req.params.id) })
-      .then((result) => {
-        res.send(result.deletedCount > 0);
+    app.get("/service/:id", (req, res) => {
+      servicesCollection
+        .find({
+          _id: ObjectId(req.params.id)
+        })
+        .toArray((err, items) => {
+          res.send(items);
+        });
+    });
+
+    app.post("/addService", (req, res) => {
+      const newService = req.body;
+      servicesCollection.insertOne(newService).then((result) => {
+        res.send(result.insertedCount > 0);
       });
-  });
-
-  //********* */
-  // Reviews
-  //******** */
-
-  const reviewsCollection = client
-    .db(process.env.DB_NAME)
-    .collection("reviews");
-
-  app.post("/addReview", (req, res) => {
-    const newBooking = req.body;
-    reviewsCollection.insertOne(newBooking).then((result) => {
-      res.send(result.insertedCount > 0);
     });
-  });
 
-  app.get("/reviews", (req, res) => {
-    reviewsCollection.find().toArray((err, items) => {
-      res.send(items);
+    app.delete("/deleteService/:id", (req, res) => {
+      servicesCollection
+        .deleteOne({
+          _id: ObjectId(req.params.id)
+        })
+        .then((result) => {
+          res.send(result.deletedCount > 0);
+        });
     });
-  });
 
-  //********** */
-  const adminCollection = client.db(process.env.DB_NAME).collection("admin");
+    //********* */
+    // Reviews
+    //******** */
 
-  app.post("/admin", (req, res) => {
-    const userEmail = req.body.email;
-    console.log(userEmail);
-    adminCollection.find({ email: userEmail }).toArray((err, admin) => {
-      res.send(admin.length > 0);
+    const reviewsCollection = client
+      .db(process.env.DB_NAME)
+      .collection("reviews");
+
+    app.post("/addReview", (req, res) => {
+      const newBooking = req.body;
+      reviewsCollection.insertOne(newBooking).then((result) => {
+        res.send(result.insertedCount > 0);
+      });
     });
-  });
 
-  app.post("/makeAdmin", (req, res) => {
-    const newAdmin = req.body;
-    adminCollection.insertOne(newAdmin).then((result) => {
-      res.send(result.insertedCount > 0);
+    app.get("/reviews", (req, res) => {
+      reviewsCollection.find().toArray((err, items) => {
+        res.send(items);
+      });
     });
-  });
-  // perform actions on the collection object
-  //   client.close();
-});
+
+    //********** */
+    const adminCollection = client.db(process.env.DB_NAME).collection("admin");
+
+    app.post("/admin", (req, res) => {
+      const userEmail = req.body.email;
+      console.log(userEmail);
+      adminCollection.find({
+        email: userEmail
+      }).toArray((err, admin) => {
+        res.send(admin.length > 0);
+      });
+    });
+
+    app.post("/makeAdmin", (req, res) => {
+      const newAdmin = req.body;
+      adminCollection.insertOne(newAdmin).then((result) => {
+        res.send(result.insertedCount > 0);
+      });
+    });
+
+    await client.db("admin").command({
+      ping: 1
+    });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+
 
 app.listen(process.env.PORT || 5055, (err) => {
   console.log("Listening");
